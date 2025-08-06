22e831b10c33bd04555bfcd0729da9273e5e4370
chinmay-joshi_cytiva <chinmay-joshi_cytiva>
2025-08-06 17:04:28 +0200

    06.08

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
-import fs from "fs";
-import http from "http";
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
 //Sync
 const text = fs.readFileSync("./txt/input.txt","utf-8")
@@ -28,9 +29,29 @@ fs.writeFileSync("./txt/output.txt", textOutput);
 
 // console.log("Reading...")
 
+ const data = fs.readFileSync(__dirname +"/dev-data/data.json", "utf-8");
+ const dataObject = JSON.parse(data);
+
 //Server
 const server = http.createServer((req,res) => {
-    res.end("Hello from the server!");
+    console.log(req.url)
+
+    const pathName = req.url;
+    if(pathName === "/" || pathName === "/home") {
+        res.end("Home")
+    }
+    if(pathName === "/api") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(dataObject));
+    }
+    else if(pathName === "/about") {
+            res.end("About")
+    }
+    else {
+        res.writeHead(404)
+        res.end("<h1>Hello from the server!</h1>");
+    }
+   
 });
 server.listen(8000,"localhost", () => {
     console.log("Server is running on port 8000");
